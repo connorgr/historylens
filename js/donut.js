db67556775435fbad7249e0dfba1a26cf344ad2a
connor <connor@cs.brown.edu>
2012-12-04 16:53:45 -0500

    Force directed donuts!

--- js/donut.js
+++ js/donut.js
@@ -14,7 +14,7 @@ function donutsViz (container) {
 
  var svgId = 'svgDonutsViz' + container.id;
 
- var svg = d3.select(container).append('svg')
+ this.svg = d3.select(container).append('svg')
    .attr('height', this.height)
    .attr('id', svgId)
    .attr('width', this.width)
@@ -23,13 +23,112 @@ function donutsViz (container) {
    .style('height', this.height)
    .style('width', this.width);
  // Test
- svg.append('circle')
+ this.svg.append('circle')
     .style('fill', '#cc0000')
     .attr('r', 30)
     .attr('cx', 30)
     .attr('cy', 30);
 
- function drawTestDonut () {
+ drawTestDonutOrig(150, 50, this.svg);
+ drawTestDonutOrig(400, 50, this.svg);
+
+ ///////////////////////////////////////////////////////////////////////////////
+ // FORCE-DIRECTED LAYOUT
+ var testJson = {'nodes':[{ 'name': 'Italy'}, { 'name': 'UK'}],
+                 'links':[{'source': 1, 'target': 0, 'value': 1}] };
+ var nodes = [],
+     links = [];
+
+
+ var force = d3.layout.force()
+         .gravity(.05)
+         .distance(100)
+         .charge(-100)
+         .size([this.width/2, this.height/2]);
+
+ force.nodes(testJson.nodes)
+   .links(testJson.links)
+   .start();
+
+ var link = this.svg.selectAll('.link')
+         .data(testJson.links)
+         .enter().append('line')
+           .attr('class', 'link');
+
+ var node = this.svg.selectAll('.node')
+         .data(testJson.nodes)
+         .enter().append('g')
+           .attr('class', 'node')
+           .call(force.drag);
+ node.append('text').text('Test.');
+ drawTestDonut(node, this.svg); 
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("transform", function(d) {
+      return "translate(" + d.x + "," + d.y + ")"; });
+  }); 
+
+ ///////////////////////////////////////////////////////////////////////////////
+ // TEST FUNCTIONS
+ function drawTestDonut(node, svg) {
+  var data = [{ num:'50' }, { num:'6' }, { num:'6' }, { num:'6' }, { num:'6' }, {num:'3'}, {num:'1'}, { num:'6' }, {num:'10'} ];
+   var radius = 50;
+   var color = d3.scale.ordinal().range(['#98abc5', '#8a89a6', '#7b6888',
+       '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
+   var arc = d3.svg.arc().outerRadius(radius).innerRadius(radius - radius/3);
+   var pie = d3.layout.pie()
+               .sort(null)
+               .value(function(d) {
+                 return d.num;
+               });
+
+   var g = node.selectAll('.arc').data(pie(data)).enter().append('g')
+              .attr('class', 'arc');
+   g.append('path').attr('d', arc)
+     .style('fill', function(d) { return color(d.value); });
+   g.append('text')
+     .attr('transform', function(d) { return 'translate('+arc.centroid(d)+')'})
+     .attr('dy', '.35em')
+     .attr('text-anchor', 'middle')
+     .text(function(d) { return d.num });
+
+   var dropShadow = svg.append('svg:defs')
+     .append('svg:filter')
+     .attr('id', 'dropShadow')
+       .append('svg:feGaussianBlur').attr('stdDeviation', 2.5)
+       .append('svg:feOffset').attr('result', 'offOut').attr('in','SourceAlpha')
+           .attr('dx', 20).attr('dy', 20)
+       .append('svg:feBlend').attr('in', 'SourceGraphic').attr('in2', 'blurOut')
+           .attr('mode', 'normal');
+
+   node.append('circle')
+     .attr('filter', 'url(#dropShadow)')
+     .attr('r', radius - radius/3)
+     .style('fill', '#fff')
+     .style('opacity', .8)
+     .style('stroke', '#ccc')
+     .style('stroke-width', 3);
+// NOTE: an optimization would use an svg filter to prevent having to
+//  render twice
+   node.append('circle')
+     .attr('r', radius - radius/3)
+     .style('fill', '#fff')
+     .style('stroke', '#dedede')
+     .style('stroke-width', 1);
+
+   node.append('text')
+     .text('Test Title')
+     .attr('class', 'donutCenterText')
+     .attr('text-anchor', 'middle');
+ }
+
+ 
+ function drawTestDonutOrig (x, y, svg) {
    var data = [{ num:'50' }, { num:'6' }, { num:'6' }, { num:'6' }, { num:'6' }, {num:'3'}, {num:'1'}, { num:'6' }, {num:'10'} ];
    var radius = 50;
    var color = d3.scale.ordinal().range(['#98abc5', '#8a89a6', '#7b6888',
@@ -41,7 +140,8 @@ function donutsViz (container) {
                  return d.num;
                });
    var testG = svg.append('g')
-                  .attr('transform','translate(' + radius*3 + ',' + radius+')');
+                  .attr('transform','translate(' + x + ',' + y + ')');
+//                  .attr('transform','translate(' + radius*3 + ',' + radius+')');
    var g = testG.selectAll('.arc').data(pie(data)).enter().append('g')
               .attr('class', 'arc');
    g.append('path').attr('d', arc)
@@ -80,6 +180,6 @@ function donutsViz (container) {
      .text('Test Title')
      .attr('class', 'donutCenterText')
      .attr('text-anchor', 'middle');
+   return testG;
  }
- drawTestDonut();
 }
