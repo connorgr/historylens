8f265de7c2b56eee5775a5524dcd5f4261f3e79d
Ubuntu <ubuntu@domU-12-31-39-14-21-74.compute-1.internal>
2012-12-09 17:11:52 +0000

    Added backend files

---
+++ data/new_data/get_items_from_zotero.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+curl "https://api.zotero.org/groups/50280/items?format=keys&key=Gq3JKFsWe1rQPdaHiOFOPxLj" > itemKeys
+
+zotero_key="Gq3JKFsWe1rQPdaHiOFOPxLj"
+zotero_format="csljson"
+echo -n "" > items
+echo -n "" > itemTags
+
+while read key; do
+  curl "https://api.zotero.org/groups/50280/items/${key}?format=${zotero_format}&key=${zotero_key}" >> items
+  echo "" >> items
+  curl "https://api.zotero.org/groups/50280/items/${key}/tags?key=${zotero_key}" >> itemTags
+  echo "" >> itemTags
+done < itemKeys
