83e8fc5faf7624fb72e2541b06ac0cd67cc194a3
tacitia <lillian.g621@gmail.com>
2012-12-11 16:50:21 -0500

    temporarily moving the data management functions into timeline.viz

--- js/timeline.js
+++ js/timeline.js
@@ -62,7 +62,7 @@ function timelineViz (container) {
 
 //    var dataManager = new dataMngr();
 
-    getSummaryDataByTime(-90, 90, -180, 180, 1800, 2010, this);
+    getSummaryDataByTime(-90, 90, -180, 180, 1800, 2010);
 
     function updateView() {
 
@@ -215,6 +215,57 @@ function timelineViz (container) {
         }
         return result;
     }
+
+    function getSummaryDataByTime(minLat, maxLat, minLng, maxLng, minYear, maxYear) {
+        console.log("summary");
+        getData(minLat, maxLat, minLng, maxLng, minYear, maxYear, binByTime);
+
+//        var timeline = getData(minLat, maxLat, minLng, maxLng, minYear, maxYear).timeline;
+//        console.log(timeline);
+//        var aggregatedTime = countAggregator(timeline);
+//        return timeline;
+    }
+
+    function binByTime(data) {
+        console.log(data);
+        console.log(viz);
+        updateView();
+    }
+
+    function countAggregator(data) {
+        var result = {};
+        for (var mainKey in data) {
+            console.log(mainKey);
+            console.log(data[mainKey]);
+            var values = data[mainKey];
+            var count = 0;
+            for (var subKey in values) {
+                count += values[subKey];
+            }
+            result[mainKey] = count;
+        }
+        console.log(result);
+        return result;
+    }
+
+
+    function getData(minLat, maxLat, minLng, maxLng, minYear, maxYear, callback) {
+        console.log("Getting data from php...");
+        var filterJSON = JSON.stringify({min_latitude: minLat, max_latitude: maxLat, min_longitude: minLng, max_longitude: maxLng, min_year: minYear, max_year: maxYear});
+        $.get("/vs/php/query.php",
+                {"q" : filterJSON},
+                function(data) {
+                    console.log(data);
+                    callback(data);
+                },
+                'json')
+         .success(function(data) { console.log("success"); })
+         .error(function(e) { 
+            console.log("error"); 
+            console.log(e.responseText); 
+         });
+    }
+    
 }
 
 
