5f8486e0dbde164d1ecec12d963c8f5805ce1c0f
tacitia <lillian.g621@gmail.com>
2012-12-11 22:18:47 -0500

    Start working on map data management

--- js/map.js
+++ js/map.js
@@ -14,48 +14,6 @@ function mapViz (container) {
 
     google.maps.event.addListener(map, 'zoom_changed', setActiveLocations);
 
-    var overlay = new google.maps.OverlayView();
-
-    var activeLocations = continents;
-
-    // Add the container when the overlay is added to the map.
-    overlay.onAdd = function() {
-        var layer = d3.select(this.getPanes().overlayLayer).append("div")
-            .attr("class", "markers");
-
-        // Draw each marker as a separate SVG element.
-        // We could use a single SVG, but what size would it have?
-        overlay.draw = function() {
-            var projection = this.getProjection(),
-                padding = 50;
-
-            layer.selectAll("svg")
-                .data(activeLocations, function(d) { console.log(d.name); return d.name; })
-                .exit().remove();
-
-            var marker = layer.selectAll("svg")
-                .data(activeLocations, function(d) { console.log(d.name); return d.name; })                
-                .each(transform) // update existing markers
-                .enter().append("svg:svg")
-                .each(transform)
-                .attr("class", "markers");
-
-            var jsonData = null;
-            drawDonut(marker, layer, jsonData);
-     
-            function transform(d) {
-                d = new google.maps.LatLng(d.lat, d.lng);
-                d = projection.fromLatLngToDivPixel(d);
-                return d3.select(this)
-                    .style("left", (d.x - padding) + "px")
-                    .style("top", (d.y - padding) + "px");
-            }
-        };
-    };
-
-    // Bind our overlay to the map…
-    overlay.setMap(map);
-
     function setActiveLocations() {
                 // Need further improvement so that we can determine 
             // the set of countries / places to show
@@ -66,6 +24,94 @@ function mapViz (container) {
                 activeLocations = cities;
             }
     }
+
+    function updateView() {
+        var overlay = new google.maps.OverlayView();
+
+        var activeLocations = continents;
+
+        // Add the container when the overlay is added to the map.
+        overlay.onAdd = function() {
+            var layer = d3.select(this.getPanes().overlayLayer).append("div")
+                .attr("class", "markers");
+
+            // Draw each marker as a separate SVG element.
+            // We could use a single SVG, but what size would it have?
+            overlay.draw = function() {
+                var projection = this.getProjection(),
+                    padding = 50;
+
+                layer.selectAll("svg")
+                    .data(activeLocations, function(d) { console.log(d.name); return d.name; })
+                    .exit().remove();
+
+                var marker = layer.selectAll("svg")
+                    .data(activeLocations, function(d) { console.log(d.name); return d.name; })                
+                    .each(transform) // update existing markers
+                    .enter().append("svg:svg")
+                    .each(transform)
+                    .attr("class", "markers");
+
+                var jsonData = null;
+                drawDonut(marker, layer, jsonData);
+         
+                function transform(d) {
+                    d = new google.maps.LatLng(d.lat, d.lng);
+                    d = projection.fromLatLngToDivPixel(d);
+                    return d3.select(this)
+                        .style("left", (d.x - padding) + "px")
+                        .style("top", (d.y - padding) + "px");
+                }
+            };
+        };
+
+        // Bind our overlay to the map…
+        overlay.setMap(map);
+    }
+
+    function getSummaryDataByLoc(minLat, maxLat, minLng, maxLng, minYear, maxYear) {
+        console.log("summary");
+        getData(minLat, maxLat, minLng, maxLng, minYear, maxYear, binByLoc);
+    }
+
+    function binByLoc(data) {
+        var summary = countAggregator(data.map);
+        updateView(summary);
+    }
+
+    function countAggregator(data) {
+        var result = {};
+        for (var mainKey in data) {
+            console.log(mainKey);
+            console.log(data[mainKey]);
+            var values = data[mainKey];
+            var count = 0;
+            for (var subKey in values) {
+                count += values[subKey];
+            }
+            result[mainKey] = count;
+        }
+        console.log(result);
+        return result;
+    }
+
+
+    function getData(minLat, maxLat, minLng, maxLng, minYear, maxYear, callback) {
+        console.log("Getting data from php...");
+        var filterJSON = JSON.stringify({min_latitude: minLat, max_latitude: maxLat, min_longitude: minLng, max_longitude: maxLng, min_year: minYear, max_year: maxYear});
+        $.get("/vs/php/query.php",
+                {"q" : filterJSON},
+                function(data) {
+                    console.log(data);
+                    callback(data);
+                },
+                'json')
+         .success(function(data) { console.log("success"); })
+         .error(function(e) { 
+            console.log("error"); 
+            console.log(e.responseText); 
+         });
+    }    
 }
 
 
