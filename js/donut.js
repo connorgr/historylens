e256e6f895ebca0cf69d787b2572b57ce0bcba37
connor <connor@cs.brown.edu>
2012-12-12 15:44:33 -0500

    hacking

--- js/donut.js
+++ js/donut.js
@@ -22,7 +22,7 @@ function drawDonut(json) {
       pie = d3.layout.pie().sort(null).value(function(d) { return d.num; });
 
   var loc = document.createElementNS("http://www.w3.org/2000/svg", "g");
-  var idDonut = 'donut' + JSON.stringify(json);
+  var idDonut = 'donut';// + JSON.stringify(json);
   loc.setAttribute('id', idDonut);
   var g = d3.select('#'+idDonut).selectAll('.arc').data(pie(data)).enter().append('g')
             .attr('class', 'arc');
