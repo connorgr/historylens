2058c43f3ac8a80a3467e53c5ef2cdff9beadeba
tacitia <lillian.g621@gmail.com>
2012-12-12 21:30:47 -0500

    z

--- js/misc.js
+++ js/misc.js
@@ -5,7 +5,7 @@
     function populateOptions() {
         var numOptions = options.length;
         for (var i = 0; i < numOptions; ++i) {
-            $('#topicSelect').append(new Option(options[i], i, false, false));
+            $('#topicSelect').append(new Option(options[i], options[i], false, false));
         }
         $('.chzn-select').chosen({allow_single_deselect: true});
         $('#topicSelect').change(topicSearchInput);
@@ -13,12 +13,15 @@
     }
 
     function topicSearchInput() {
-        console.log("???");
         var topic = this.value;
         console.log(topic);
         getDocuments(-90, 90, -180, 180, startYear, endYear, [topic]);
     }
 
-    function populateDocList() {
-        
+    function populateDocList(documents) {
+        console.log(documents);
+        var numDocs = documents.length;
+        for (var i = 0; i < numDocs; ++i) {
+            d3.select('#docList').append('tr');
+        }
     }
