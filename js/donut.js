e1f8e9f72948cde7e10ed1a03fbcb46b8dda5cb6
connor <connor@cs.brown.edu>
2012-12-12 22:15:24 -0500

    Visual ∆

--- js/donut.js
+++ js/donut.js
@@ -21,7 +21,7 @@ function drawDonut(d3Selection, data) {
       donutColorsList = ['#8DD3C7', '#FFFFB3', '#BEBADA', '#FB8072', '#80B1D3',
           '#FDB462', '#B3DE69', '#FCCDE5', '#D9D9D9', 'BC80BD'],
       donutColors = d3.scale.ordinal().range(donutColorsList),
-      arc = d3.svg.arc().outerRadius(radius - 5).innerRadius(radius - radius/3),
+      arc = d3.svg.arc().outerRadius(radius - 3).innerRadius(radius - radius/3),
       pie = d3.layout.pie().sort(null).value(function(d) { return d.num; });
 
   var loc = d3Selection.append('g');
@@ -46,7 +46,7 @@ function drawDonut(d3Selection, data) {
       .attr("x", radius/3)//function(d) { return d._children ? -8 : -48; }) /*the position of the text (left to right)*/
       .attr("y", 30) /*the position of the text (Up and Down)*/
       .attr("height", "100px")
-      .attr("width", 1.5*(radius - radius/3))
+      .attr("width", "70px")
       .append("xhtml:body")
       .append("p")
       .text(function(d) { return d.placeName; })
