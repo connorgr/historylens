7f3ffd0a0b58aff391b952a803616d2de7ac2a94
tacitia <lillian.g621@gmail.com>
2012-12-12 20:49:17 -0500

    z

--- js/misc.js
+++ js/misc.js
@@ -6,7 +6,8 @@
         console.log("options");
         var numOptions = options.length;
         for (var i = 0; i < numOptions; ++i) {
-            $('topicSelect').append(new Option(options[i], i, false, false));
+            console.log("append");
+            $('#topicSelect').append(new Option(options[i], i, false, false));
         }
         $('.chzn-select').chosen({allow_single_deselect: true});
         optionsLoaded = true;
