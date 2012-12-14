ae0f89f523ff45c3c0a8d8c389b2ba2f4e75e53c
connor <connor@cs.brown.edu>
2012-12-13 22:51:32 -0500

    !

--- js/legend.js
+++ js/legend.js
@@ -1,3 +1,7 @@
 d3.select('#areaLegend')
-     .append('svg')
-     .append('rect');
+    .selectAll('svg')
+    .append('svg')
+      .attr('width', '1000px')
+      .attr('height', '100px')
+      .style('background-color', '#ff0000')
+          .append('rect');
