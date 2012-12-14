95c21c79f75f9330812b5caa41600a33cc26a837
connor <connor@cs.brown.edu>
2012-12-13 23:08:37 -0500

    testin'

--- js/legend.js
+++ js/legend.js
@@ -1,10 +1,10 @@
 function initLegend() {
   console.log('Inside the making of legends.');
   console.log($('#areaLegend'));
+
   $('#areaLegend').append('<p>Test</p>');
 
   d3.select('#areaLegend')
-    .selectAll('svg')
     .append('svg')
       .attr('width', '1000px')
       .attr('height', '100px')
