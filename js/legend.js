55d198a187f88edb110cb700332a4cd04b9fe27f
connor <connor@cs.brown.edu>
2012-12-13 23:06:02 -0500

    Take threeve

--- js/legend.js
+++ js/legend.js
@@ -1,14 +1,13 @@
-d3.select('#areaLegend')
+function initLegend() {
+  console.log('Inside the making of legends.');
+  console.log($('#areaLegend'));
+  $('#areaLegend').append('<p>Test</p>');
+
+  d3.select('#areaLegend')
     .selectAll('svg')
     .append('svg')
       .attr('width', '1000px')
       .attr('height', '100px')
       .style('background-color', '#ff0000')
           .append('rect');
-
-
-$.ready(function () {
-  console.log('Inside the making of legends.');
-  console.log($('#areaLegend'));
-  $('#areaLegend').append('<p>Test</p>');
 });
