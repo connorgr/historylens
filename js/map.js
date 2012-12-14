66946b01e46e732ab9616afdde5ff71bd9126c55
tacitia <lillian.g621@gmail.com>
2012-12-13 22:45:31 -0500

    z

--- js/map.js
+++ js/map.js
@@ -71,7 +71,8 @@
       // Draw each marker as a separate SVG element.
       // We could use a single SVG, but what size would it have?
       overlay.draw = function() {
-      
+        d3.selectAll('.markers').remove();
+              
         var layer = d3.select(this.getPanes().overlayLayer).append("div")
           .attr("class", "markers");
 
