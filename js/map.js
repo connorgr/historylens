5960c81792bcf3b79d1be9e675f2f7dbde7e5161
tacitia <lillian.g621@gmail.com>
2012-12-13 22:05:05 -0500

    trying to make donuts update

--- js/map.js
+++ js/map.js
@@ -66,12 +66,15 @@
 
     // Add the container when the overlay is added to the map.
     overlay.onAdd = function() {
-      var layer = d3.select(this.getPanes().overlayLayer).append("div")
-          .attr("class", "markers");
 
       // Draw each marker as a separate SVG element.
       // We could use a single SVG, but what size would it have?
       overlay.draw = function() {
+        d3.selectAll('markers').remove();
+      
+        var layer = d3.select(this.getPanes().overlayLayer).append("div")
+          .attr("class", "markers");
+
         var projection = this.getProjection(),
             padding = 50;
         
